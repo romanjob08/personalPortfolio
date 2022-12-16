5138fb7410c9f78afb28a0b224c2fd5d6fa99559
Roman Mykhasiv <mihasivroman@gmail.com>
2022-12-16 22:42:34 +0200

    feature: added scss for App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {Navbar} from './components'
 import {About, Footer, Header, Skills, Testimonial, Work} from './container';
+import './App.scss'
 
 const App = () => {
     return (
